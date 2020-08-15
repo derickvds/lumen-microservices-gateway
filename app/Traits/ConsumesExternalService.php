6aec1ac360ddbcc7e74b5d1d73e2b24141c1a89d
Derick van der Sandt <derickvds@gmail.com>
2020-08-15 20:55:48 +0200

    Added secret tokens in header

--- app/Traits/ConsumesExternalService.php
+++ app/Traits/ConsumesExternalService.php
@@ -17,6 +17,10 @@ trait ConsumesExternalService
             'base_uri' => $this->baseUri,
         ]);
 
+        if (isset($this->secret)) {
+            $headers['Authorization'] = $this->secret;
+        }
+
         $response = $client->request($method, $requestUrl, ['form_params' => $formParams, 'headers' => $headers]);
 
         return $response->getBody()->getContents();
--- config/services.php
+++ config/services.php
@@ -2,11 +2,13 @@
 
 return [
     'authors' => [
-        'base_uri' => env('AUTHORS_SERVICE_BASE_URL')
+        'base_uri' => env('AUTHORS_SERVICE_BASE_URL'),
+        'secret' => env('AUTHORS_SERVICE_SECRET'),
     ],
 
     'books' => [
-        'base_uri' => env('BOOKS_SERVICE_BASE_URL')
+        'base_uri' => env('BOOKS_SERVICE_BASE_URL'),
+        'secret' => env('BOOKS_SERVICE_SECRET'),
     ]
 ];
 
