2d53850fd5e7890ac62c85092786bf652d42dd15
Derick van der Sandt <derickvds@gmail.com>
2020-08-15 10:35:15 +0200

    Added author and book connections

--- app/config/services.php
+++ app/config/services.php
@@ -1,7 +1,13 @@
 <?php
 
 return [
-    
+    'authors' => [
+        'base_uri' => env('AUTHORS_SERVICE_BASE_URL')
+    ],
+
+    'books' => [
+        'base_uri' => env('BOOKS_SERVICE_BASE_URL')
+    ]
 ];
 
 ?>
